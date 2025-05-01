5253fc5167f5cc43135c46344644769e478b9335
Doğanay URGUPLUOGLU <doganayurgupluoglu@gmail.com>
2025-05-01 12:05:54 +0300

    added sounds some animations history

--- src/js/app.js
+++ src/js/app.js
@@ -18,18 +18,19 @@ adayForm.addEventListener("submit", e => {
     e.preventDefault();
 
     // console.log("submit");
-    if(adayInput.value.trim() === ""){
-        alertMessage.style.opacity = "1";
-        return
-    }
-    else{
-        alertMessage.style.opacity = "0";
-    }
+    // if(adayInput.value.trim() === ""){
+    //     alertMessage.style.opacity = "1";
+    //     return
+    // }
+    // else{
+    //     alertMessage.style.opacity = "0";
+    // }
     const li = yeniAday();
     adayListesi.appendChild(li);
     adayInput.value = "";
     loadBtns();
     counterAndRate();
+    hideWinnerContainer();
     
 });
 
@@ -63,36 +64,65 @@ function randomWinner() {
 function showWinner(e) {
     e.preventDefault();
     const kazananContainer = document.querySelector(".kazanan-container");
+    const winnerSound = document.getElementById("winner-sound");
+    const winnerHistoryList = document.querySelector(".winner-history"); 
     const kazanan = randomWinner();
-    currentWinner = kazanan;
-    if(!kazanan){
+    currentWinner = kazanan; 
+
+    if (!kazanan) {
         return;
     }
 
+    winnerSound.currentTime = 0;
+    winnerSound.play().catch(err => {
+        console.warn("Ses çalınamadı:", err);
+    });
     const kazananP = document.createElement("p");
     kazananP.className = "kazanan";
-    kazananP.textContent = `'${kazanan.textContent}' Tebrikler!`;
+    const winnerName = kazanan.firstChild.textContent.trim();  
+    kazananP.textContent = `'${winnerName}' Tebrikler!`;
 
-    kazananContainer.innerHTML = "";
+
+    kazananContainer.innerHTML = "";  
     kazananContainer.appendChild(kazananP);
+
+
+    const historyItem = document.createElement("li");
+    historyItem.className = "history-item";
+    historyItem.textContent = winnerName; 
+    const deleteIcon = document.createElement("i");
+    deleteIcon.className = "fa-solid fa-x deleteIcon";
+    deleteIcon.addEventListener("click", removeAday);
+    
+    historyItem.appendChild(deleteIcon);
+    winnerHistoryList.appendChild(historyItem);
+
+
     kazananContainer.style.display = "flex";
+
+
     const deleteWinnerBtn = document.createElement("button");
     deleteWinnerBtn.innerText = "Kazananı listeden çıkar";
-    deleteWinnerBtn.addEventListener("click", deleteWinner)
-
+    deleteWinnerBtn.className = "delete-winner-btn";
+    deleteWinnerBtn.addEventListener("click", deleteWinner);
     kazananContainer.appendChild(deleteWinnerBtn);
-}
+
+    showWinnerContainer();
+} 
 
 function clearList(e) {
     e.preventDefault()
     adayListesi.innerHTML = "";
+    document.querySelector(".winner-history").innerHTML = ""; 
     counterAndRate();
     loadBtns();
+    currentWinner = null;  
+    hideWinnerContainer();
 }
 function loadBtns(){
     const adaylar = document.querySelectorAll(".aday");
-    clearBtn.style.display = adaylar.length > 0 ? "inline-block": "none";
-    resultBtn.style.display = adaylar.length > 1 ? "inline-block": "none";
+    clearBtn.disabled = adaylar.length === 0;
+    resultBtn.disabled = adaylar.length <= 1;
 }
 
 
@@ -121,5 +151,29 @@ function deleteWinner(){
         currentWinner = null;
         counterAndRate();
         loadBtns();
+        const deleteSound = document.querySelector("#delete-sound");
+        deleteSound.currentTime = 0;
+        deleteSound.play().catch(err => {
+            console.warn("Ses çalınamadı:", err);
+        });
+
+        const deleteBtn = document.querySelector(".delete-winner-btn");
+        deleteBtn.textContent ="Kazanan Listeden Silindi";
+        deleteBtn.classList.add("disabled");
+        deleteBtn.disabled = true;
+        hideWinnerContainer();
     }
+}
+
+function hideWinnerContainer() {
+    setTimeout(() => {
+        const kazananContainer = document.querySelector(".kazanan-container");
+            kazananContainer.classList.remove("show");
+    }, 100);
+}
+function showWinnerContainer() {
+    setTimeout(() => {
+        const kazananContainer = document.querySelector(".kazanan-container");
+        kazananContainer.classList.add("show");
+    }, 0);
 }
